f4d58edf50e4cad404c466999964e6c690bfa6ab
nadcantik <nadtasyafaizahaqilah@gmail.com>
2025-05-07 14:30:19 +0700

    backend

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "macok";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi berhasil!";
+}
+?>
